9dbab62f869eab35d8a828ab80d0e14ad7835bf8
Albert Fang <hr00090241@gmail.com>
2016-12-17 11:36:21 +0800

    practice vue

---
+++ src/components/Users.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="users">
+    <h1>Users</h1>
+    <form v-on:submit="addUser">
+      <input type="text" v-model="newUser.name" placeholder="Enter Name"/>
+      <br>
+      <input type="text" v-model="newUser.email" placeholder="Enter Email"/>
+      <br>
+      <input type="submit" value="Submit"/>
+    </form>
+    
+    <ul>
+        <li v-for="user in users"> 
+          <input type="checkbox" class="toggle" v-model="user.contacted">
+          <span :class="{contacted: user.contacted}">
+            {{user.name}} : {{user.email}} <button v-on:click="deleteUser(user)">x</button>
+          </span>
+      </li> 
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'users',
+    data () {
+      return {
+        newUser: {},
+        users: []
+      }
+    },
+    methods: {
+      addUser: function (e) {
+        e.preventDefault()
+        this.users.push({
+          name: this.newUser.name,
+          email: this.newUser.email,
+          contacted: false
+        })
+      },
+      deleteUser: function (user) {
+        this.users.splice(this.users.indexOf(user), 1)
+      }
+    },
+    created: function () {
+      this.$http.get('https://jsonplaceholder.typicode.com/users')
+        .then(function (res) {
+          this.users = res.data
+        })
+    }
+  }
+</script>
+
+<style scoped>
+  .contacted {
+    text-decoration: line-through;
+  }
+</style>
---
+++ src/components/test.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="test">
+    <input type="text" v-model="title"/><br />
+    <h1>{{title}}</h1>
+    <p v-if="showName">{{user.first_name}}</p>
+    <p v-else="showName">No one</p>
+    <ul>
+      <li v-for="item in items">{{item.title}}</li>
+    </ul>
+    <button v-on:click="greet('Hello there')">Say Greeting</button>
+    <input type="text" v-on:keyup="pressKey" v-on:keyup.enter="hitEnter">
+    <hr>
+    <label for="" >First Name</label>
+    <input type="text" v-model="user.first_name"/> 
+    <label for="">Last Name</label>
+    <input type="text" v-model="user.last_name"/>
+    <h3>{{fullName}}</h3>
+    <h2>{{msg}}</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'test',
+    props: {
+      msg: {
+        type: String,
+        default: 'Foobar'
+      }
+    },
+    data () {
+      return {
+        title: 'Hello World',
+        user: {
+          first_name: 'Albert',
+          last_name: 'Fang'
+        },
+        showName: true,
+        items: [
+          {title: 'item one'},
+          {title: 'item two'},
+          {title: 'item three'}
+        ]
+      }
+    },
+    methods: {
+      greet: function (greeting) {
+        console.log(greeting)
+      },
+      pressKey: function () {
+        console.log('presskey')
+      },
+      hitEnter: function () {
+        console.log('you hit enter')
+      }
+    },
+    computed: {
+      fullName: function () {
+        return `${this.user.first_name} ${this.user.last_name}`
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
